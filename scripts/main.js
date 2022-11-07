c28343f56153e9f5e54d51cdd2512cbeff028f1b
Younnes Chebli <younnesc@gmail.com>
2022-11-07 16:58:46 +0100

    update

--- scripts/main.js
+++ scripts/main.js
@@ -1,134 +1,12 @@
-const createButton = document.getElementById("create");
-const cards = document.getElementById("cards");
+const taskForm = document.getElementById("task-form");
+const tasks = document.getElementById("tasks");
+const taskList = [];
+let nextID = 0;
 const max = Number.MAX_VALUE;
-//0: none, 1: urgence, 2: name
-var sortType = 0;
-//0: none, 1: todo, 2: doing, 3: done
-var filterType = 0;
-const urgenceSort = document.getElementById("urgence-sort");
-const all = document.getElementById("all");
-const nameSort = document.getElementById("name-sort");
-const todoFilter = document.getElementById("todo");
-const doingFilter = document.getElementById("doing");
-const doneFilter = document.getElementById("done");
-var actualTasks = document.querySelectorAll(".card");
+const lsTasks = localStorage.getItem("tasks");
 
-const disableFilters = () => {
-    todoFilter.disabled = true;
-    doingFilter.disabled = true;
-    doneFilter.disabled = true;
-    all.disabled = true;
-};
-
-const activateFilters = () => {
-    todoFilter.disabled = false;
-    doingFilter.disabled = false;
-    doneFilter.disabled = false;
-    all.disabled = false;
-};
-
-const checkFilters = () => {
-    if(actualTasks.length == 0) {
-        disableFilters();
-    } else {
-        activateFilters();
-    }
-};
-
-const resetDisplay = () => {
-    for(const task of actualTasks) {
-        task.style.display = "block";
-    }
-};
-
-const filterByTodo = () => {
-    resetDisplay();
-    for(const task of actualTasks) {
-        if(task.querySelector("select :nth-child(2)").selected == true) {
-            task.style.display = "block";
-        } else {
-            task.style.display = "none";
-        }
-    }
-};
-
-const filterByDoing = () => {
-    resetDisplay();
-    for(const task of actualTasks) {
-        if(task.querySelector("select :nth-child(3)").selected == true) {
-            task.style.display = "block";
-        } else {
-            task.style.display = "none";
-        }
-    }
-};
-
-const filterByDone = () => {
-    resetDisplay();
-    for(const task of actualTasks) {
-        if(task.querySelector("select :nth-child(4)").selected == true) {
-            task.style.display = "block";
-        } else {
-            task.style.display = "none";
-        }
-    }
-};
-
-const filter = () => {
-    switch(filterType) {
-        case 1: filterByTodo();
-        break;
-        case 2: filterByDoing();
-        break;
-        case 3: filterByDone();
-        break;
-    }
-};
-
-const sortByName = (tasks) => {
-    const tasksArray = Array.from(tasks);
-    cards.innerHTML = "";
-    
-    tasksArray.sort((a, b) => {
-        if(a.querySelector("input").value < b.querySelector("input").value) {
-            return -1;
-        } else if (a.querySelector("input").value > b.querySelector("input").value) {
-            return 1;
-        } else {
-            return 0;
-        }
-    });
-    
-    for(const task of tasksArray) {
-        cards.append(task);
-    }
-};
-
-const sortByUrgence = (tasks) => {
-    const tasksArray = Array.from(tasks);
-    cards.innerHTML = "";
-
-    tasksArray.sort((a, b) => {
-        return Number(a.querySelector("p").getAttribute("days-left")) - Number(b.querySelector("p").getAttribute("days-left"));
-    });
-
-    for(const task of tasksArray) {
-        cards.append(task);
-    }
-} ;
-
-const sort = () => {
-    switch(sortType) {
-        case 1: sortByUrgence(actualTasks);
-        break;
-        case 2: sortByName(actualTasks);
-        break;
-    }
-};
-
-const refresh = () => {
-    sort();
-    filter();
+const save = () => {
+    localStorage.setItem("tasks", JSON.stringify(taskList));
 };
 
 const msToDays = (ms) => {
@@ -144,44 +22,38 @@ const displayRemaining = (e) => {
     remaining.innerHTML = "";
     const dateStringIn = e.target.value;
     const dateInMs = Date.parse(dateStringIn);
-    let daysRemaining;
 
     if(!isBefore(dateInMs)) {
-        daysRemaining = msToDays(dateInMs) - msToDays(Date.now());
+        const daysRemaining = msToDays(dateInMs) - msToDays(Date.now());
         remaining.innerHTML = `in ${daysRemaining} days!`;
         remaining.setAttribute("days-left", `${daysRemaining}`);
     }
 
-    refresh();
+    //refresh();
 };
 
-const resetFilter = () =>{
-    resetDisplay();
-    filterType = 0;
-    todoFilter.checked = false;
-    doingFilter.checked = false;
-    doneFilter.checked = false;
-    all.checked = true;
-    refresh();
-};
-
-const createTask = () => {
+const generateTask = (task) => {
     const card = document.createElement("div");
+    card.setAttribute("ID", task.ID);
     card.classList.add("card");
-    resetFilter();
+    //resetFilter();
 
     const nameInput = document.createElement("input");
     nameInput.type = "text";
     nameInput.classList.add("name");
     nameInput.placeholder = "task name";
+    nameInput.value = task.name;
+    nameInput.addEventListener("input", (e) => {
+        const i = card.getAttribute("ID");
+        taskList[i].name = e.target.value;
+        save();
+    });
 
     const statusSelect = document.createElement("select");
     statusSelect.classList.add("status-select");
-    const status = document.createElement("option");
-    status.value = "";
-    status.innerHTML = "STATUS";
-    status.selected= true;
-    status.disabled = true;
+    const noStatus = document.createElement("option");
+    noStatus.value = "noStatus";
+    noStatus.innerHTML = "NO STATUS";
     const todoOption = document.createElement("option");
     todoOption.value = "todo";
     todoOption.innerHTML = "TO DO";
@@ -191,26 +63,43 @@ const createTask = () => {
     const doneOption = document.createElement("option");
     doneOption.value = "done";
     doneOption.innerHTML = "DONE";
-    statusSelect.append(status, todoOption, doingOption, doneOption);
+    statusSelect.value = task.statusSelect
+    statusSelect.append(noStatus, todoOption, doingOption, doneOption);
+    statusSelect.addEventListener("change", (e) => {
+
+    });
 
     const description = document.createElement("textarea");
     description.classList.add("description");
     description.cols = "25";
     description.rows = "5";
     description.placeholder = "description";
+    description.innerHTML = task.description;
+    description.addEventListener("input", (e) => {
+
+    });
 
     const dueDateC = document.createElement("div");
     dueDateC.classList.add("due-date");
     const dueDateLabel = document.createElement("label");
     dueDateLabel.innerHTML = "Due date";
     const dueDateInput = document.createElement("input");
-    dueDateInput.addEventListener("change", displayRemaining);
     dueDateInput.type = "date";
+    dueDateInput.value = task.dueDate;
     dueDateC.append(dueDateLabel, dueDateInput);
+    dueDateInput.addEventListener("change", displayRemaining);
+    dueDateInput.addEventListener("change", (e) => {
+
+    });
 
     const remaining = document.createElement("p");
     remaining.classList.add("remaining");
     remaining.setAttribute("days-left", `${max}`);
+    if(!isBefore(Date.parse(task.dueDate)) && task.dueDate != "") {
+        const daysRemaining = msToDays(Date.parse(task.dueDate)) - msToDays(Date.now());
+        remaining.innerHTML = `in ${daysRemaining} days!`;
+        remaining.setAttribute("days-left", `${daysRemaining}`);
+    }
 
     const trashC = document.createElement("div");
     trashC.classList.add("trash-c");
@@ -219,55 +108,35 @@ const createTask = () => {
     trashFont.classList.add("fa-solid", "fa-trash", "trash");
     trashB.append(trashFont);
     trashC.append(trashB);
+    trashB.addEventListener("click", (e) => {
+
+    });
 
     card.append(nameInput, statusSelect, description, dueDateC, remaining, trashC);
 
-    cards.append(card);
+    tasks.append(card);
 
-    actualTasks = document.querySelectorAll(".card");
-    refresh();
-    checkFilters();
+    //refresh();
+    //checkFilters();
 };
 
-createButton.addEventListener("click", createTask);
+const generateTasks = (taskList) => {
+    tasks.innerHTML = "";
+    for(const task of taskList) {
+        generateTask(task);
+    }
+} ;
 
-const enableSortByName = () => {
-    sortType = 2;
-    refresh();
-};
+taskForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const formData = Object.fromEntries(new FormData(taskForm));
+    formData.ID = nextID;
+    nextID++;
+    taskList.push(formData);
+    save();
+    generateTasks(taskList);
+});
 
-const enableSortByUrgence = () => {
-    sortType = 1;
-    refresh();
-};
-
-urgenceSort.addEventListener("click", enableSortByUrgence);
-nameSort.addEventListener("click", enableSortByName);
-
-const enableTodoFilter = () => {
-    filterType = 1;
-    todoFilter.checked = true;
-    all.checked = false;
-    refresh();
-};
-
-const enableDoingfilter = () => {
-    filterType = 2;
-    doingFilter.checked = true;
-    all.checked = false;
-    refresh()
-};
-
-const enableDoneFilter = () => {
-    filterType = 3;
-    doneFilter.checked = true;
-    all.checked = false;
-    refresh();
-};
-
-all.addEventListener("click", resetFilter);
-todoFilter.addEventListener("click", enableTodoFilter);
-doingFilter.addEventListener("click", enableDoingfilter);
-doneFilter.addEventListener("click", enableDoneFilter);
-
-checkFilters();
+if(lsTasks != null) {
+    generateTasks(JSON.parse(lsTasks));   
+}
